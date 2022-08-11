cec47f553020e8d9332869847e6ee3bbcf13d4d7
mlehna <malte.lehna@iee.fraunhofer.de>
2022-08-11 11:39:17 +0200

    reducing actions

--- curriculumagent/scripts/run_rllib_with_advanced_model.py
+++ curriculumagent/scripts/run_rllib_with_advanced_model.py
@@ -33,7 +33,7 @@ def get_configs() -> Tuple[SeniorEnvRllib, AdvancedCustomModel]:
     """
     Agent_Path = Path(__file__).parent.parent.parent
     actions = [Agent_Path / "action_space"/ "new" / "actionspace_208_poth.npy"]
-    target = Agent_Path / 'senior'/"JuniorModel_new"
+    target = Agent_Path / 'senior'/"JuniorModel"
 
     e_c = {"action_space_path": actions,
            "data_path": Agent_Path.parent/"training_data_track1",
